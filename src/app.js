a7f8a8c2c06df1a0b254e50d3438fa81ce211393
Victoria Moreno <vmlopez4@gmail.com>
2021-09-10 13:52:10 +0100

    header menu for mobile added functionality

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
-const hamburguerMenu = document.querySelector("#btnHamburguer")
+let hamburguerMenu = document.querySelector("#btnHamburguer")
+let mobileMenu = document.querySelector(".header_menu");
 
 hamburguerMenu.addEventListener("click", function(){
-    console.log("Hello")
     if (hamburguerMenu.classList.contains("opened")){
         hamburguerMenu.classList.remove("opened");
+        mobileMenu.classList.remove("mobile");
     }else{
         hamburguerMenu.classList.add("opened");
+        mobileMenu.classList.add("mobile");
     }
-
-
 });
+
