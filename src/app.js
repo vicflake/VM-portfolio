566484a3dcae7ebe2f4380dbbf4c570b6ee9907b
Victoria Moreno <vmlopez4@gmail.com>
2021-09-10 14:50:55 +0100

    mobile menu to close when anchor link clicked

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 let hamburguerMenu = document.querySelector("#btnHamburguer")
 let mobileMenu = document.querySelector(".header_menu");
+let body = document.querySelector("body");
+let menuAnchors = document.querySelectorAll(".header_menu a")
+
+console.log(menuAnchors);
 
 hamburguerMenu.addEventListener("click", function(){
     if (hamburguerMenu.classList.contains("opened")){
         hamburguerMenu.classList.remove("opened");
         mobileMenu.classList.remove("mobile");
+        body.classList.remove("noscroll");
+
+
     }else{
         hamburguerMenu.classList.add("opened");
         mobileMenu.classList.add("mobile");
+        body.classList.add("noscroll");
     }
 });
+
+menuAnchors.forEach(function(element){
+    element.addEventListener("click", function(){
+        if(hamburguerMenu.classList.contains("opened")){
+            mobileMenu.classList.remove("mobile");
+            body.classList.remove("noscroll");
+            hamburguerMenu.classList.remove("opened");
+        }
+    })
+})
 
